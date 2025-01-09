d31101cea2ff6dfdc3c04e61cc60c0a71860295d
IkBenJur <jurspammer@gmail.com>
2025-01-09 21:43:41 +0100

    Get photo by ID

--- src/routers/photo.ts
+++ src/routers/photo.ts
@@ -12,6 +12,27 @@ photoRouter.get("/", async (req: Request, res: Response) => {
 });
 
 photoRouter.get("/:id", async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const photo = await prisma.photo.findUnique({
+      where: { id: Number(id) },
+    });
+  
+    if (!photo) {
+      res.json({ error: `No file with id: ${id}` });
+      return;
+    }
+    
+    const captions = await prisma.captionsOnPhotos.findMany({
+        where: { photo: photo }
+    })
+
+    res.json({
+        ...photo,
+        captions
+    })
+})
+
+photoRouter.get("/file/:id", async (req: Request, res: Response) => {
   const { id } = req.params;
   const photo = await prisma.photo.findUnique({
     where: { id: Number(id) },
